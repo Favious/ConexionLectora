5d5b212ee51071137b8f464b6f08af7790dfbeda
BillieVC <manuel.vargas2013.jmvc@gmail.com>
2021-06-03 18:28:17 -0400

    cambios necesarioos

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
 void main() {
-   runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Conexion Lectora',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter PDF',
       theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
       ),
-      initialRoute: LoginPage.id,
-      routes: {
-        LoginPage.id : (context)=> LoginPage() ,
-      },
-      );
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  String url= "http://conorlastowka.com/book/CitationNeededBook-Sample.pdf";
+  String pdfasset ="assets/sample.pdf";
+  PDFDocument _doc;
+  bool _loading;
+  @override
+  void initState() {
+    
+    super.initState();
+    _initPdf();
+  }
+
+   _initPdf() async {
+     
+      setState(() {
+        _loading=true;
+      });
+      PDFDocument docu= await PDFDocument.fromURL(url);
+      final doc = docu;
+      
+      setState(() {
+        _doc=doc;
+        _loading=false;
+      });
+      
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter PDF Demo"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),): PDFViewer(document: _doc),
+    );
   }
 }
