e398aad6e25a78c81302f9b1b4db55381266f78a
BillieVC <manuel.vargas2013.jmvc@gmail.com>
2021-06-07 19:59:02 -0400

    nueva implementacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  bool _isLoading = true;
+  PDFDocument document;
+
+  @override
+  void initState() {
+    super.initState();
+    loadDocument();
+  }
+
+  loadDocument() async {
+    document = await PDFDocument.fromURL('https://germarmu.files.wordpress.com/2014/09/resumen-cien-ac3b1os-de-soledad-mc3a1rquez.pdf');
+
+
+    setState(() => _isLoading = false);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter PDF',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterPluginPDFViewer'),
+        ),
+        body: Center(
+          child: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : PDFViewer(
+                  document: document,
+                  zoomSteps: 1,
+                  //uncomment below line to preload all pages
+                   lazyLoad: false,
+                  // uncomment below line to scroll vertically
+                   scrollDirection: Axis.vertical,
+
+                  //uncomment below code to replace bottom navigation with your own
+                   navigationBuilder:
+                      (context, page, totalPages, jumpToPage, animateToPage) {
+                    return ButtonBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.first_page),
+                          onPressed: () {
+                            jumpToPage(page: 0);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            animateToPage(page: page - 2);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.arrow_forward),
+                          onPressed: () {
+                            animateToPage(page: page);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.last_page),
+                          onPressed: () {
+                            jumpToPage(page: totalPages - 1);
+                          },
+                        ),
+                      ],
+                    );
+                  }, 
+                ),
+        ),
+      ),theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
-      home: HomePage(),
-    );
-  }
-}
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  
-  String url= "http://conorlastowka.com/book/CitationNeededBook-Sample.pdf";
-  String pdfasset ="assets/sample.pdf";
-  PDFDocument _doc;
-  bool _loading;
-  @override
-  void initState() {
-    
-    super.initState();
-    _initPdf();
-  }
-
-   _initPdf() async {
-     
-      setState(() {
-        _loading=true;
-      });
-      PDFDocument docu= await PDFDocument.fromURL(url);
-      final doc = docu;
-      
-      setState(() {
-        _doc=doc;
-        _loading=false;
-      });
-      
-    }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter PDF Demo"),),
-      body: _loading ? Center(child: CircularProgressIndicator(),): PDFViewer(document: _doc),
     );
   }
 }
